9360376c9b94d8c594d0e63223ff7100601c8b08
wendellbarcelos <wendell_barcelos2@hotmail.com>
2020-12-13 14:19:01 -0300

    Projeto finalizado

---
+++ backend/data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "8HUi90wooIA",
+        title: "Illustration Style Website - Speed Design and Code ( HTML & CSS )",
+        duration: "14 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "JCKp7m9ALE0",
+        title: "Dota2 Dashboard Navigation Menu | ReactJS + HTML + CSS | Speed Coding ( Part 1 )",
+        duration: "11 min",
+        price: "Free"
+    },
+    {
+        id: "vpn8UUkfj6A",
+        title: "Web Design // Retro Flat Design // Speed Art + Speed Code",
+        duration: "10 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "fabwoIpHxaY",
+        title: "Web Design — Star Wars Design — Speed Art + Speed Code",
+        duration: "19 min",
+        price: "Free"
+    },
+    {
+        id: "gbBXIzBNkVo",
+        title: "Html/CSS: Animated login form css | Speed coding",
+        duration: "5 min",
+        price: "Free"
+    },
+    {
+        id: "JDiurjhpOXA",
+        title: "Speed Coding | HTML, CSS, JS - Calculator",
+        duration: "9:15 min",
+        price: "Free",
+        featured: true
+    }
+]
--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,52 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function(req, res){
-    return res.render("about")
+    const about = {
+        avatar_url: "https://media-exp1.licdn.com/dms/image/C4E03AQFn1u-ULICipw/profile-displayphoto-shrink_200_200/0/1606535871059?e=1611792000&v=beta&t=YDMTnUIY7Efe805rkqzmsyUYdAXcfofJvAFOwIQvl1k",
+        name: "Wendell Barcelos",
+        role: "Full stack engineer", 
+        description:`Programador full-stack, apaixonado pelas melhores tecnologias de desenvolvimento back-end, front-end e mobile.
+        Colaborador da <a href='https://react-brasil-slack.herokuapp.com/'   target="_blank">React Brazil</a>.`,
+        links: [
+            { name: "Github", url: "https://github.com/wendellbarcelos"},
+            { name: "Linkedin", url: "https://www.linkedin.com/in/wendell-barcelos/"},
+            { name: "Gmail", url: "mailto:wendellbarcelos.wb@gmail.com"}
+        ]
+    }
+
+    return res.render("about", { about })
 })
 
 server.get("/videos", function(req, res){
-    return res.render("videos")
+
+    return res.render("videos", { items: videos })
+})
+
+server.get("/video", function(req, res){
+    const id = req.query.id
+
+    const video = videos.find(function(video){
+        return video.id == id
+    })
+
+    if (!video) {
+        return res.send("Video not found!")
+    }
+
+    return res.render("video", { item: video })
 })
 
 server.listen(5000, function(){
